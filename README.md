aa1ea17ff865dd8f5ba16339a5a905cb423c4c84
nipil <nipil@users.noreply.github.com>
2022-10-14 17:28:01 +0200

    watch gpio and notify

--- README.md
+++ README.md
@@ -1,12 +1,5 @@
 # GPIO change Notifier for ESP8266 devices with ESP Framework
 
 TODO:
-- wifi provisionning
-- select GPIO in options
-- select notification URL
-- HTTPS or HTTP
-- setup GPIO
-- read GPIO
-- detect change
-- notify
 - keapalive
+- manage disconnections
--- main/main.c
+++ main/main.c
@@ -7,6 +7,7 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <string.h>
+#include <time.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
@@ -21,11 +22,18 @@
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
+#include "driver/gpio.h"
+
+#include "esp_http_client.h"
+
+const int WAIT_LOOP_MS = 100;
+const int DEBOUNCE_MS = 100;
+
 bool connected = false;
 
-static const char *TAG = "wifi station";
+static const char *TAG = "gcn";
 
-static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+static void gcn_wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
 {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
     {
@@ -54,13 +62,13 @@ void wifi_init_sta(void)
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &gcn_wifi_event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &gcn_wifi_event_handler, NULL));
 
     wifi_config_t wifi_config = {
         .sta = {
-            .ssid = CONFIG_ESP_WIFI_SSID,
-            .password = CONFIG_ESP_WIFI_PASSWORD},
+            .ssid = CONFIG_GCN_WIFI_SSID,
+            .password = CONFIG_GCN_WIFI_PASSWORD},
     };
 
     /* Setting a password implies station will connect to all security modes including WEP/WPA.
@@ -79,16 +87,176 @@ void wifi_init_sta(void)
     ESP_LOGI(TAG, "wifi_init_sta finished.");
 }
 
+esp_err_t gcn_http_event_handler(esp_http_client_event_t *evt)
+{
+    static char *output_buffer; // Buffer to store response of http request from event handler
+    static int output_len;      // Stores number of bytes read
+
+    switch (evt->event_id)
+    {
+    case HTTP_EVENT_ERROR:
+        ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+        break;
+
+    case HTTP_EVENT_ON_CONNECTED:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+        break;
+
+    case HTTP_EVENT_HEADER_SENT:
+        ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+        break;
+
+    case HTTP_EVENT_ON_HEADER:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+        break;
+
+    case HTTP_EVENT_ON_DATA:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+        /*
+         *  Check for chunked encoding is added as the URL for chunked encoding used in this example returns binary data.
+         *  However, event handler can also be used in case chunked encoding is used.
+         */
+        if (!esp_http_client_is_chunked_response(evt->client))
+        {
+            // If user_data buffer is configured, copy the response into the buffer
+            if (output_buffer == NULL)
+            {
+                output_buffer = (char *)malloc(esp_http_client_get_content_length(evt->client) + 1);
+                output_len = 0;
+                if (output_buffer == NULL)
+                {
+                    ESP_LOGE(TAG, "Failed to allocate memory for output buffer");
+                    return ESP_FAIL;
+                }
+            }
+            memcpy(output_buffer + output_len, evt->data, evt->data_len);
+            output_len += evt->data_len;
+        }
+
+        break;
+    case HTTP_EVENT_ON_FINISH:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+        if (output_buffer != NULL)
+        {
+            // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
+            // ESP_LOG_BUFFER_HEX(TAG, output_buffer, output_len);
+            output_buffer[output_len] = '\0';
+            ESP_LOGD(TAG, "content : %s", output_buffer);
+
+            time_t *srv_time = evt->user_data;
+            if (srv_time != NULL)
+                *srv_time = atol(output_buffer);
+
+            free(output_buffer);
+            output_buffer = NULL;
+        }
+        output_len = 0;
+        break;
+
+    case HTTP_EVENT_DISCONNECTED:
+        ESP_LOGD(TAG, "HTTP_EVENT_DISCONNECTED");
+        if (output_buffer != NULL)
+        {
+            free(output_buffer);
+            output_buffer = NULL;
+        }
+        output_len = 0;
+        break;
+    }
+    return ESP_OK;
+}
+
+void notify_server(int gpio_value)
+{
+    // build URI
+    char buf[512];
+    sprintf(buf, "%s/%s", CONFIG_GCN_NOTIFY_BASE_URL, CONFIG_GCN_HOST_NAME);
+
+    time_t srv_time;
+
+    // prepare client request
+    esp_http_client_config_t config = {
+        .url = buf,
+        .method = HTTP_METHOD_POST,
+        .transport_type = HTTP_TRANSPORT_OVER_TCP,
+        .event_handler = gcn_http_event_handler,
+        .user_data = &srv_time,
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+    assert(client != NULL);
+
+    // build post data
+    time_t now = time(NULL);
+    sprintf(buf, "time=%lu&gpio=%i&value=%i", now, CONFIG_GCN_WATCH_GPIO_NUMBER, gpio_value);
+    esp_http_client_set_post_field(client, buf, strlen(buf));
+    ESP_LOGD(TAG, "query parameters : %s", buf);
+
+    // request
+    esp_err_t err = esp_http_client_perform(client);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "HTTP POST request failed: %s", esp_err_to_name(err));
+        return;
+    }
+    ESP_LOGI(TAG, "HTTP POST Status = %d, content_length = %d", esp_http_client_get_status_code(client), esp_http_client_get_content_length(client));
+
+    // sync time
+    if (abs(srv_time - now) > 60)
+    {
+        ESP_LOGI(TAG, "Synchronizing local clock (%li) to server clock (%li)", now, srv_time);
+        struct timeval tv = {
+            .tv_sec = srv_time,
+            .tv_usec = 0,
+        };
+        if (settimeofday(&tv, NULL) != 0)
+            ESP_LOGW(TAG, "Could not update local clock");
+    }
+}
+
 void app_main()
 {
+    if (!GPIO_IS_VALID_GPIO(CONFIG_GCN_WATCH_GPIO_NUMBER))
+    {
+        ESP_LOGE(TAG, "Invalid GPIO number %i", CONFIG_GCN_WATCH_GPIO_NUMBER);
+        abort();
+    }
+
     ESP_ERROR_CHECK(nvs_flash_init());
 
     ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
     wifi_init_sta();
 
+    gpio_config_t io_conf = {};
+    io_conf.pin_bit_mask = (1ULL << CONFIG_GCN_WATCH_GPIO_NUMBER);
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
+
+    bool last_state = gpio_get_level(CONFIG_GCN_WATCH_GPIO_NUMBER);
     while (true)
     {
-        vTaskDelay(1000 / portTICK_RATE_MS);
-        ESP_LOGI(TAG, "connected=%i", connected);
+        // idle
+        vTaskDelay(WAIT_LOOP_MS / portTICK_RATE_MS);
+
+        // detect change
+        bool new_state = gpio_get_level(CONFIG_GCN_WATCH_GPIO_NUMBER);
+        if (new_state == last_state)
+            continue;
+
+        // soft-debounce
+        vTaskDelay(DEBOUNCE_MS / portTICK_RATE_MS);
+
+        // check that change was not a transient
+        new_state = gpio_get_level(CONFIG_GCN_WATCH_GPIO_NUMBER);
+        if (new_state == last_state)
+            continue;
+
+        // TODO: if not connected, put it in the back-log
+
+        // input changed for good, so notify
+        last_state = new_state;
+        ESP_LOGI(TAG, "GPIO %i changed to %i", CONFIG_GCN_WATCH_GPIO_NUMBER, last_state);
+        notify_server(new_state);
     }
 }
